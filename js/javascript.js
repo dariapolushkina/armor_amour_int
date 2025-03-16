2144827473b3b965ff7158afe18a6d2234b163fd
dariapolushkina <mioyasumi@gmail.com>
2025-03-16 20:00:08 +0300

    ok

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,23 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const bw = document.querySelector('.knight.bw');
+//     const pink = document.querySelector('.knight.pink');
+//     const clickKnight = document.querySelector('.knight_bw .knight_svg'); 
+
+//     clickKnight.addEventListener("click", function () {
+//         if (bw.style.opacity === "1" || bw.style.opacity === "") {
+//             bw.style.opacity = "0";
+//             bw.style.pointerEvents = "none";
+//             pink.style.opacity = "1";
+//             pink.style.pointerEvents = "auto";
+//         } else {
+//             bw.style.opacity = "1";
+//             bw.style.pointerEvents = "auto";
+//             pink.style.opacity = "0";
+//             pink.style.pointerEvents = "none";
+//         }
+//     });
+// });
+
 document.addEventListener("DOMContentLoaded", function () {
     const bw = document.querySelector('.knight.bw');
     const pink = document.querySelector('.knight.pink');
@@ -9,14 +29,25 @@ document.addEventListener("DOMContentLoaded", function () {
             bw.style.pointerEvents = "none";
             pink.style.opacity = "1";
             pink.style.pointerEvents = "auto";
+
+            clickKnight.style.opacity = "0"; 
+            clickKnight.style.pointerEvents = "auto"; 
         } else {
             bw.style.opacity = "1";
             bw.style.pointerEvents = "auto";
             pink.style.opacity = "0";
             pink.style.pointerEvents = "none";
+
+            clickKnight.style.opacity = "1"; 
+            clickKnight.style.pointerEvents = "auto"; 
         }
     });
 });
+
+
+
+
+
 
 
 document.addEventListener("DOMContentLoaded", function () {
